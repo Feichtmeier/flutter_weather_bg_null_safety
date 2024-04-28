24c67a872c6d7d16e207838c0cf6584f9034327f
Feichtmeier <frederik.feichtmeier@gmail.com>
2024-04-28 20:56:12 +0200

    Add simple test

--- test/flutter_weather_bg_test.dart
+++ test/flutter_weather_bg_test.dart
@@ -1,5 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_weather_bg_null_safety/flutter_weather_bg.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: SizedBox.square(
+            dimension: 500,
+            child: WeatherBg(
+              weatherType: WeatherType.sunny,
+              width: 500,
+              height: 500,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    // Verify that our counter starts at 0.
+    expect(find.byType(WeatherBg), findsOneWidget);
+  });
 }
